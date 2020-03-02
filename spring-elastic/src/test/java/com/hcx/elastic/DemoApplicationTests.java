b3a8c227d5b219917c6e6349261b71bc23219b09
HeCCXX <985701799@qq.com>
2020-03-02 11:41:11 +0800

    jest操作es，利用jestClient

--- spring-elastic/src/test/java/com/hcx/elastic/DemoApplicationTests.java
+++ spring-elastic/src/test/java/com/hcx/elastic/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.hcx.elastic;
 
+import com.hcx.elastic.bean.Article;
 import com.hcx.elastic.bean.Book;
 import com.hcx.elastic.repository.BookRepository;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class DemoApplicationTests {
+
+    @Autowired
+    JestClient jestClient;
 
     @Autowired
     BookRepository bookRepository;
@@ -24,5 +34,54 @@ public class DemoApplicationTests {
 
         bookRepository.index(book);
     }
+
+    /**
+    * 利用jest  api 对es进行操作，Index，Search等操作搜索、插入
+    * @param
+    * @return void
+    * @exception       
+    **/
+  @Test
+  public void test(){
+      //1 、 给es中索引（保存）一个文档
+      Article article = new Article();
+      article.setId(1);
+      article.setAuthor("hcx");
+      article.setContent("hgg");
+      article.setTitle("it is a good message!");
+
+      //构建一个索引功能
+      Index index = new Index.Builder(article).index("hgg").type("message").build();
+      try {
+          //执行
+          jestClient.execute(index);
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+
+  //搜索功能
+  @Test
+  public void test2(){
+      //查询表达式
+      String json = "{\n" +
+              "\"query\":{\n" +
+              " \"match\":{\n" +
+              "\"content\":\"good\"\n" +
+              "}\n" +
+              "}\n" +
+              "}";
+
+
+      Search search = new Search.Builder(json).addIndex("hgg").addType("message").build();
+
+      try {
+          SearchResult result = jestClient.execute(search);
+          System.out.println(result.getJsonString());
+
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
 
 }
