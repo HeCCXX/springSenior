bf38fd57c7287411eb1f8b3fe22858d055a46a9c
HeCCXX <985701799@qq.com>
2020-03-09 16:08:16 +0800

    springcloud  Init commit

---
+++ springcloud-consumer/src/test/java/com/hcx/consumer/SpringcloudconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcx.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
